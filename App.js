9c20e0c39284870014ce2a44b658e38329c850af
chairulanam612 <chairulanam612@gmail.com>
2021-12-02 13:47:19 +0700

    coba lagi

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>coba</Text>
+      <Text>coba lagi</Text>
       <StatusBar style="auto" />
     </View>
   );
